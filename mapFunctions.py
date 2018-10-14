534de321a5dabb072259c35bf1ecfa447acde682
Jenibluere <jenibluere@gmail.com>
2018-10-14 11:51:29 +0100

    Spring Cleaning

--- mapFunctions.py
+++ mapFunctions.py
@@ -1,37 +1,3 @@
-# #key functions
-
-# import logging
-# import math
-
-# travelPriority = {
-# 	"snitch" : 1,
-# 	"snitchGoal" : 2,
-# 	"health" : 3,
-# 	"ammo" : 4,
-# 	"chaseEnemy" : 5,
-# 	"idleMovement" : 6
-# }
-
-# def GetHeading(x1,y1,x2,y2):
-# 	heading = math.degrees(math.atan2(y2-y1,x2-x1))
-# 	heading = math.fmod(heading-360,360)
-# 	print(heading)
-# 	return int(math.fabs(heading))
-	
-# def GetDistance(x1,y1,x2,y2):
-# 	displacement_x=x2-x1
-# 	displacement_y=y2-y1
-# 	return int(math.sqrt(displacement_x**2 + displacement_y**2))
-
-# def move_to_position(ServerMessageTypes,GameServer,xpos,ypos,desiredxpos,desiredypos,body_heading=0,distance_to_coord=0,movementType="idle"):
-# 	body_heading = GetHeading(xpos, ypos, desiredxpos,desiredypos )	
-# 	distance_to_coord = GetDistance(xpos, ypos, desiredxpos, desiredypos)
-	
-	
-# 	logging.info('Going to origin')
-# 	GameServer.sendMessage(ServerMessageTypes.TURNTOHEADING, {"Amount": body_heading})
-# 	GameServer.sendMessage(ServerMessageTypes.MOVEFORWARDDISTANCE , {"Amount": distance_to_coord})
-
 #key functions
 
 import logging
@@ -72,7 +38,7 @@ def move_to_position(ServerMessageTypes,GameServer,xpos,ypos,desiredxpos,desired
 		currentPriority = tryPriority
 		body_heading = GetHeading(xpos, ypos, desiredxpos,desiredypos )	
 		distance_to_coord = GetDistance(xpos, ypos, desiredxpos, desiredypos)
-		
+
 		GameServer.sendMessage(ServerMessageTypes.TURNTOHEADING, {"Amount": body_heading})
 		GameServer.sendMessage(ServerMessageTypes.MOVEFORWARDDISTANCE , {"Amount": distance_to_coord})
 
